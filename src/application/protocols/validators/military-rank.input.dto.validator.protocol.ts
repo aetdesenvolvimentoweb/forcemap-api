f567f69cc3406d22dc1c1cf554cb1033e7ef1add
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-22 17:39:51 -0300

    feat: create military rank input dto validator interface

---
+++ src/application/protocols/validators/military-rank.input.dto.validator.protocol.ts
@@ -0,0 +1,5 @@
+import type { MilitaryRankInputDTO } from "@domain/dtos";
+
+export interface MilitaryRankInputDTOValidatorProtocol {
+  validate(data: MilitaryRankInputDTO, idToIgnore?: string): Promise<void>;
+}
