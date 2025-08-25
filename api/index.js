cbcf32666fc268bf91437c2868cd6d632361ad1f
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-25 17:46:41 -0300

    refactor: test ci render deploy

--- api/index.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-// api/index.js
-const app = require("../dist/main/server.js");
-module.exports = app;
--- src/main/server.ts
+++ src/main/server.ts
@@ -12,6 +12,7 @@ route.get("/", (req: Request, res: Response) => {
 
 app.use(route);
 
-app.listen(3333, () => "server running on port 3333");
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`server running on port ${port}`));
 
 export default app;
